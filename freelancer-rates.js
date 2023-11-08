9277bb68b6d9f2b68f6fc6172a93ae816778d470
ramiromtz <ramiro@MacBook-Air-de-Ramiro.local>
2023-11-08 11:25:04 -0600

    Exercism solutions JS

---
+++ freelancer-rates.js
@@ -0,0 +1,41 @@
+// Get those rates calculated!
+
+export const TOTAL_DAYS_PER_MONTH = 22;
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return ratePerHour * 8;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour));
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  let extraDays =  numDays % TOTAL_DAYS_PER_MONTH;
+  let totalMonths = (numDays - extraDays) / TOTAL_DAYS_PER_MONTH;
+  let discountPerMonth = 1 - discount;
+  let pricePerExtraDays = dayRate(ratePerHour) * extraDays;
+  let pricePerMonth = (dayRate(ratePerHour) * TOTAL_DAYS_PER_MONTH) * discountPerMonth;
+  return Math.ceil(pricePerMonth * totalMonths + pricePerExtraDays);
+}
